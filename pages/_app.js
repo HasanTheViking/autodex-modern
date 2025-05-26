e5670da5cc6dc3266eafb6552e58348352e15089
HasanTheViking <163563125+HasanTheViking@users.noreply.github.com>
2025-05-26 20:57:07 +0200

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,18 @@
+import '../styles/globals.css'
+import { CartProvider } from '../components/CartContext'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
+export default function App({ Component, pageProps }) {
+  return (
+    <CartProvider>
+      <div className="flex flex-col min-h-screen bg-gray-100 text-gray-900">
+        <Header />
+        <main className="flex-grow container mx-auto px-4 py-8">
+          <Component {...pageProps} />
+        </main>
+        <Footer />
+      </div>
+    </CartProvider>
+  )
+}
