41069c78f6e5cc1e3eff7abc8df6915ba17e9a8b
HasanTheViking <163563125+HasanTheViking@users.noreply.github.com>
2025-05-26 20:52:31 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#E60000",
+        dark: "#000000",
+        light: "#FFFFFF"
+      }
+    }
+  },
+  plugins: []
+};
