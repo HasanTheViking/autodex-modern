975ccc5b301449ccc55770ea039f2228aada603e
HasanTheViking <163563125+HasanTheViking@users.noreply.github.com>
2025-05-27 21:59:05 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import { CartProvider } from '../components/CartContext'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import { AuthProvider } from '../contexts/AuthContext'
 
 export default function App({ Component, pageProps }) {
   return (
