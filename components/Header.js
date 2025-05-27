7b18e6f5a509ad8362aa879e121439acd717623b
HasanTheViking <163563125+HasanTheViking@users.noreply.github.com>
2025-05-27 21:34:47 +0200

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import { useCart } from './CartContext'
- import { useAuth } from '../contexts/AuthContext'
+import { useAuth } from '../contexts/AuthContext'
 
 export default function Header() {
   const { cart, points } = useCart()
