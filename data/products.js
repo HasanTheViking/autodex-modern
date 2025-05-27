8ddc3537239e730af26320a5cf99eaba73c735c2
HasanTheViking <163563125+HasanTheViking@users.noreply.github.com>
2025-05-27 14:12:42 +0200

    Create data/products.js

---
+++ data/products.js
@@ -0,0 +1,51 @@
+// data/products.js
+export const products = [
+  {
+    id: 1,
+    name: 'Šampón na auto – 1 liter',
+    price: 12.99,
+    description: 'Koncentrovaný šampón pre dokonalý lesk a ochranu laku.',
+    category: 'kozmetika',
+    image: '/images/shampoo.jpg'
+  },
+  {
+    id: 2,
+    name: 'Ochranný vosk / sealant (250 ml)',
+    price: 14.99,
+    description: 'Dodáva laku dlhodobú ochranu a vysoký lesk.',
+    category: 'kozmetika',
+    image: '/images/wax.jpg'
+  },
+  {
+    id: 3,
+    name: 'Mikrovláknové utierky (3 ks)',
+    price: 9.99,
+    description: 'Sada kvalitných utierok pre šetrné sušenie a voskovanie.',
+    category: 'kozmetika',
+    image: '/images/microfiber.jpg'
+  },
+  {
+    id: 4,
+    name: 'Sada zimných pneumatík 16″ (4 ks)',
+    price: 199.00,
+    description: 'Bezpečné pneumatiky pre zimné podmienky.',
+    category: 'pneumatiky',
+    image: '/images/winter-tires.jpg'
+  },
+  {
+    id: 5,
+    name: 'Mobilný kompresor / inflátor',
+    price: 29.99,
+    description: 'Praktický doplnok na jednoduché dofúkanie.',
+    category: 'pneumatiky',
+    image: '/images/inflator.jpg'
+  },
+  {
+    id: 6,
+    name: 'Univerzálny držiak na telefón',
+    price: 7.99,
+    description: 'Držiak do mriežky ventilácie pre bezpečnú navigáciu.',
+    category: 'doplnky',
+    image: '/images/phone-mount.jpg'
+  },
+]
