16403aa4bd56b693d0bcd07fc4457a2d3f064a5e
HasanTheViking <163563125+HasanTheViking@users.noreply.github.com>
2025-05-27 22:14:23 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,14 @@
 // pages/_app.js
 import '../styles/globals.css'
 import { CartProvider } from '../components/CartContext'
+import { AuthProvider } from '../contexts/AuthContext'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
-import { AuthProvider } from '../contexts/AuthContext'   // <-- importuj AuthProvider
 
 export default function App({ Component, pageProps }) {
   return (
     <CartProvider>
-      <AuthProvider>                                     // <-- obal komponenty AuthProviderom
+      <AuthProvider>
         <Header />
         <Component {...pageProps} />
         <Footer />
