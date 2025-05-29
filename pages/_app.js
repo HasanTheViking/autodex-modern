d12df6c8cd52c781f144c6a2094658c4391d98b0
HasanTheViking <163563125+HasanTheViking@users.noreply.github.com>
2025-05-29 17:21:01 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 // pages/_app.js
+import React from 'react'
 import '../styles/globals.css'
 import { CartProvider } from '../components/CartContext'
 import { AuthProvider } from '../contexts/AuthContext'
@@ -7,20 +8,30 @@ import Footer from '../components/Footer'
 import CookieBanner from '../components/CookieBanner'
 
 export default function App({ Component, pageProps }) {
+  React.useEffect(() => {
+    // Globálny handler na zachytenie client-side chýb
+    window.onerror = (message, source, line, column, error) => {
+      alert(
+        `Runtime chyba:\n${message}\n` +
+        `v súbore: ${source}\n` +
+        `riadok: ${line}, stĺpec: ${column}`
+      )
+      // vráti true, aby sa prehliadač neprepísal vlastnou hláškou
+      return true
+    }
+  }, [])
+
   return (
     <CartProvider>
       <AuthProvider>
         <div className="flex flex-col min-h-screen">
-          {/* horná lišta + cookie upozornenie */}
           <Header />
           <CookieBanner />
 
-          {/* hlavný obsah (flex-grow = vyplní všetok zostávajúci priestor) */}
           <main className="flex-grow bg-gray-50 p-6">
             <Component {...pageProps} />
           </main>
 
-          {/* pätička vždy dole */}
           <Footer />
         </div>
       </AuthProvider>
